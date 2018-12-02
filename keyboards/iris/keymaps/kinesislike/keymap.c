5c82b5035a0395a5af84fb52c416edfbfec25366
UENISHI Kota <kuenishi@gmail.com>
2018-12-03 02:57:26 +0900

    it's no more kinesis-like but original

--- keyboards/iris/keymaps/kinesislike/keymap.c
+++ keyboards/iris/keymaps/kinesislike/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      GRV , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,MINS,
      TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,BSLS,
      LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
-     LSFT, Z  , X  , C  , V  , B  ,ESC,     LGUI, N , M  ,COMM,DOT ,SLSH,PLUS,
-                        LOWR,LALT, BSPC,         ENTS, SPC,RASE
+     LSFT, Z  , X  , C  , V  , B  ,ESC,     LGUI, N , M  ,COMM,DOT ,SLSH,EQL,
+                        LOWR,LALT, BSPC,         SPC,ENTS, RASE
   ),
 
   [_LOWER] = LAYOUT_kc(
@@ -47,15 +47,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          ,    ,    ,    ,    ,    ,                   ,    ,    ,    ,LBRC,RBRC,
          ,    ,    ,    ,    ,    ,               VOLU,PGUP,HOME,    , UP ,    ,
          ,    ,    ,    ,    ,    ,ESC,     RPRN,VOLD,PGDN,END ,LEFT,DOWN,RGHT,
-                        LOWR,LALT, BSPC,         ENTS, SPC,RASE
-  ),
+                        LOWR,LALT, BSPC,         SPC,ENTS, RASE
+                       ),
 
   [_RAISE] = LAYOUT_kc(
      F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
          ,    ,    ,    ,    ,    ,                   ,    ,    ,    ,LBRC,RBRC,
          ,    ,    ,    ,    ,    ,               VOLU,PGUP,HOME,    , UP ,    ,
          ,    ,    ,    ,    ,    ,LGUI,     RPRN,VOLD,PGDN,END ,LEFT,DOWN,RGHT,
-                        LOWR,LALT, BSPC,         ENTS, SPC,RASE
+                             LOWR,LALT, BSPC,         SPC,ENTS, RASE
   ),
 
   [_ADJUST] = LAYOUT(
